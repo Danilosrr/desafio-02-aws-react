27abd5adf89baceb79b7d2cf6595c4d9ff66471b
Danilosrr <danilo_srr@hotmail.com>
2024-10-01 12:58:18 -0300

    feat: componente de navegacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ComicList from './Pages/ComicList';
+import "./Assets/reset.css"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<></>}/>
-      <Route path='/' element={<></>}/>
+      <Route path='/comics' element={<ComicList/>}/>
       <Route path='/' element={<></>}/>
       <Route path='/' element={<></>}/>
       <Route path='/' element={<></>}/>
