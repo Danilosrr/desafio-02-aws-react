d2414073a5c251d415b2dffd9e044d72fc14e60b
biancassc <biancassc26@gmail.com>
2024-10-02 12:55:59 -0300

    feat : criação ItemCar, mostra itens no carrinho ou tela vazia

--- src/Components/Checkout/ItemCar.tsx
+++ src/Components/Checkout/ItemCar.tsx
@@ -1,1 +1,24 @@
 import React from "react";
+import EmptyCar from "./EmptyCar";
+import {useCar} from './CheckoutLogic';
+
+const ItemCar: React.FC = ()=>{
+    const {carItems ,removeItem} = useCar();
+    if (carItems.length ===0){
+        return <EmptyCar />;
+    }
+    return(
+        <div className="car">
+        <h1>Meu Carrinho</h1>
+        <ul>
+            {carItems.map(item=>(<li key={item.id}>
+                <span>
+                    {item.name}-R${item.price.toFixed(2)}</span>
+                    <button onClick={()=>removeItem(item.id)}>Remover</button>
+            </li>))}
+        </ul>
+        </div>
+    )
+}
+
+export default ItemCar;
