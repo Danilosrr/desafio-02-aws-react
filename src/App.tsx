4c66952f6a57a343120a1f2c165bbc0bdb47dc35
biancassc <biancassc26@gmail.com>
2024-10-02 22:08:42 -0300

    fix:apagar componentes testes que foram commitados de forma errada

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ItemCar from './Components/Checkout/ItemCar';
 import { CarProvider } from './Components/Checkout/CheckoutLogic';
-import MockAddCar from './Components/Checkout/MockAddCar';
+
 
 function App() {
   return (
     <BrowserRouter>
     <CarProvider>
     <Routes>
-      <Route path='/carrinho' element={<ItemCar/>}/>
-      <Route path='/cheio' element={<MockAddCar/>}/>
+      <Route path='/' element={<></>}/>
+      <Route path='/' element={<></>}/>
       <Route path='/' element={<></>}/>
       <Route path='/' element={<></>}/>
       <Route path='/' element={<></>}/>
