f15c3030e09fd28bc1c067972d2eb87a9b7df0cd
Alex verissimo <alexverissimo98@gmail.com>
2024-10-03 01:30:41 -0300

    feat: nova rota adicionada ao Route app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmpetyCar from './Components/Checkout/EmpetyCar';
 import SignUpForm from './Components/SignUpForm/SignUpForm';
-import ComicList from './Pages/ComicList';
+
 import ComicDetailsPage from './Pages/ComicDetails';
 import "./Assets/reset.css"
+import ComicHome from './Pages/ComicHome';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/signupforms' element={<SignUpForm />}/>
-      <Route path='/comics' element={<ComicList/>}/>
+      <Route path='/comics' element={<ComicHome/>}/>
       <Route path='/comics/:id' element={<ComicDetailsPage/>}/>
       <Route path='/' element={<></>}/>
       <Route path='/' element={<></>}/>
