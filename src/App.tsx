28da08e94cfde9cfb9e2760a2afa23651cb222dd
Danilosrr <danilo_srr@hotmail.com>
2024-10-03 18:28:42 -0300

    feat: contexto de compras, redirecionamento de checkout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import EmptyCar from './Components/Checkout/EmptyCar';
-import SignUpForm from './Components/SignUpForm/SignUpForm';
+import EmptyCar from "./Components/Checkout/EmptyCar";
+import SignUpForm from "./Components/SignUpForm/SignUpForm";
 
-import ComicDetailsPage from './Pages/ComicDetails';
-import "./Assets/reset.css"
-import ComicHome from './Pages/ComicHome';
+import ComicDetailsPage from "./Pages/ComicDetails";
+import "./Assets/reset.css";
+import ComicHome from "./Pages/ComicHome";
+import CartProvider from "./Contexts/CartContext";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/signupforms' element={<SignUpForm />}/>
-      <Route path='/comics' element={<ComicHome/>}/>
-      <Route path='/comics/:id' element={<ComicDetailsPage/>}/>
-      <Route path='/' element={<></>}/>
-      <Route path='/' element={<></>}/>
-    </Routes>
-     </BrowserRouter>
+      <CartProvider>
+        <Routes>
+          <Route path="/signupforms" element={<SignUpForm />} />
+          <Route path="/comics" element={<ComicHome />} />
+          <Route path="/comics/:id" element={<ComicDetailsPage />} />
+          <Route path="/" element={<></>} />
+          <Route path="/" element={<></>} />
+        </Routes>
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
