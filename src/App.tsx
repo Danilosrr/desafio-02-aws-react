7e6c70f392fa5dd9b6290523fbf825c674394d5b
biancassc <biancassc26@gmail.com>
2024-10-04 20:40:25 -0300

    feat : atualizando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmptyCar from './Components/Checkout/EmptyCar';
+import SignUpForm from './Components/SignUpForm/SignUpForm';
+import ComicList from './Pages/ComicHome';
+import ComicHome from "./Pages/ComicHome";
+import ComicDetailsPage from './Pages/ComicDetailsPage';
+import OrderDetailsPage from "./Pages/OrderDetails";
+import "./Assets/reset.css";
+import ComicContext from "./Contexts/ComicListContext";
+import CharacterList from './Pages/CharacterHome';
+import LoginForm from './Components/LoginForm/LoginForm';
+import CharacterContext from './Contexts/CharacterListContext';
+import { CarProvider } from './Components/Checkout/CheckoutLogic';
+import ItemCar from './Components/Checkout/ItemCar';
 
 function App() {
   return (
-    <CarProvider>
     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<></>}/>
-      <Route path='/' element={<></>}/>
-      <Route path='/' element={<></>}/>
-      <Route path='/' element={<></>}/>
-      <Route path='/' element={<></>}/>
-    </Routes>
-     </BrowserRouter>
-     </CarProvider>
+      <ComicContext>
+      <CharacterContext>
+        <CarProvider>
+          <Routes>
+            <Route path="/signupform" element={<SignUpForm />} />
+            <Route path="/comics" element={<ComicHome />} />
+            <Route path="/comics/:id" element={<ComicDetailsPage />} />
+            <Route path='/characters' element={<CharacterList/>}/>      
+            <Route path='/characters/:id' element={<ComicDetailsPage/>}/>
+            <Route path="/order" element={<OrderDetailsPage />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/carrinho" element={<ItemCar/>} />
+          </Routes>
+        </CarProvider>
+      </CharacterContext>
+      </ComicContext>
+    </BrowserRouter>
+     
   );
 }
 
