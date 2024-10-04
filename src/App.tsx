bfb1625986d327e42ccd23b0d922f761b307a947
Danilosrr <danilo_srr@hotmail.com>
2024-10-04 18:32:44 -0300

    fix: detalhes de hq e personagens em um componente

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,29 @@ import ComicList from './Pages/ComicHome';
 import ComicHome from "./Pages/ComicHome";
 import ComicDetailsPage from './Pages/ComicDetailsPage';
 import OrderDetailsPage from "./Pages/OrderDetails";
-
-import CharacterDetailsPage from './Pages/CharacterDetailsPage';
 import "./Assets/reset.css";
 import CartProvider from "./Contexts/CartContext";
 import ComicContext from "./Contexts/ComicListContext";
 import CharacterList from './Pages/CharacterHome';
+import CharacterContext from './Contexts/CharacterListContext';
 
 function App() {
   return (
     <BrowserRouter>
       <ComicContext>
+      <CharacterContext>
         <CartProvider>
           <Routes>
             <Route path="/signupforms" element={<SignUpForm />} />
             <Route path="/comics" element={<ComicHome />} />
             <Route path="/comics/:id" element={<ComicDetailsPage />} />
             <Route path='/characters' element={<CharacterList/>}/>      
-            <Route path='/characters/:id' element={<CharacterDetailsPage/>}/>
+            <Route path='/characters/:id' element={<ComicDetailsPage/>}/>
             <Route path="/order" element={<OrderDetailsPage />} />
             <Route path="/" element={<></>} />
           </Routes>
         </CartProvider>
+      </CharacterContext>
       </ComicContext>
     </BrowserRouter>
      
--- src/Components/ComicDetails/ComicDetails.tsx
+++ src/Components/ComicDetails/ComicDetails.tsx
@@ -1,150 +1,244 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import useCartContext from "../../Hooks/useCartContext";
 import { RxChevronLeft } from "react-icons/rx";
 import { MdAddShoppingCart } from "react-icons/md";
 import { request } from "../../Api/api";
-import loading from "../../Assets/loading.gif";
+import loader from "../../Assets/loading.gif";
+import { Character } from "../../types/character-types";
 import { Comic } from "../../types/comic-type";
 import "./ComicDetails.css";
 
-interface Character {
-  name: string;
-  thumbnail: { extension: string; path: string };
+interface Details {
+  comic: Comic | null;
+  comics: Comic[];
+  characters: Character[];
+  character: Character | null;
+  characterComics: Comic[];
 }
 
 export default function ComicDetails() {
   const { id } = useParams();
-  const {addToCart} = useCartContext()
   const navigate = useNavigate();
-  const [noComic, setNoComic] = useState<boolean>(false);
-  const [comic, setComic] = useState<Comic | null>(null);
-  const [comics, setComics] = useState<Comic[] | null>(null);
-  const [characters, setCharacters] = useState<Character[]>([]);
-
+  const location = useLocation();
+  const { addToCart } = useCartContext();
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState<Details>({
+    comic: null,
+    comics: [],
+    character: null,
+    characters: [],
+    characterComics: [],
+  });
 
   function handleNavigate() {
     navigate(`/comics`);
   }
 
-  function handleBuy(now?:boolean){
-    if (comic) addToCart(comic);
-    if(now) navigate('/checkout');
+  function handleBuy(now?: boolean) {
+    if (details.comic) addToCart(details.comic);
+    if (now) navigate("/checkout");
   }
 
   async function getData() {
     try {
-      const comic = await request(`/v1/public/comics/${id}`);
-      const comics = await request(`/v1/public/comics?limit=20`);
-      const characters = await request(`/v1/public/comics/${id}/characters?limit=3`);
-       
-      setComic(comic.data.results[0] as Comic);
-      setComics(comics.data.results as Comic[]);
-      setCharacters(characters.data.results as Character[]);
-      return
+      if(location.pathname.includes("comics")){
+        const comic = await request(`/v1/public/comics/${id}`);
+        const comics = await request(`/v1/public/comics?limit=20`);
+        const characters = await request(
+          `/v1/public/comics/${id}/characters?limit=3`
+        );
+
+        console.log(characters)
+        const data = {
+          ...details,
+          comic: comic.data.results[0],
+          comics: comics.data.results,
+          characters: characters.data.results,
+        };
+        setDetails(data);
+      }
+      if(location.pathname.includes("characters")){
+        const character = await request(`/v1/public/characters/${id}`);
+        const comics = await request(`/v1/public/comics?limit=20`);
+        const characterComics =await request(
+          `/v1/public/characters/${id}/comics?limit=3`
+        )
+
+        const data = {
+          ...details,
+          character: character.data.results[0],
+          comics: comics.data.results,
+          characterComics: characterComics.data.results,
+        };
+        setDetails(data);
+      }
     } catch (error) {
-      setNoComic(true);
+      setDetails({ ...details });
       console.log(error);
+    } finally{
+      setLoading(false);
     }
   }
 
   useEffect(() => {
     getData();
+    console.log(details.comic);
   }, []);
 
-  return (
-    <main className="mainDetails">
-      {comic ? (
-        <div className="container">
-          <nav onClick={handleNavigate}>
-            <RxChevronLeft color="#FF8100"/>
-            Voltar
-          </nav>
-          <div className="comic">
-            <img
-              className="cover"
-              src={comic.images.length?`${comic.images[0].path} .${comic.images[0].extension}`:
-              `${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`}
-              alt="comic cover"
-            />
-            <aside>
-              <section className="title">
-                <h2>{comic.title}</h2>
-                <p>{"$ " + comic.prices[0].price}</p>
-              </section>
-              <div className="details">
-                <article>
-                  <h3>Publicado em</h3>
-                  <p>{comic.dates[0].date.slice(0, 4)}</p>
-                </article>
-                <article>
-                  <h3>Núm. de Páginas</h3>
-                  <p>{comic.pageCount}</p>
-                </article>
-                <article>
-                  <h3>Autor</h3>
-                  <p>{comic.creators.items[0]?.name}</p>
-                </article>
-                <article>
-                  <h3>Série</h3>
-                  <p>{comic.series?.name}</p>
-                </article>
-                <article className="characters">
-                  <h3>Personagens da obra</h3>
-                  <div className="row">
-                    {characters.map((c) => {
-                      return (
-                        <figure className="character" key={c.name}>
-                          <img
-                            src={
-                              c.thumbnail.path +
-                              "/standard_medium" +
-                              "." +
-                              c.thumbnail.extension
-                            }
-                            alt="character"
-                          />
-                          <b>{c.name}</b>
-                        </figure>
-                      );
-                    })}
-                  </div>
-                </article>
-              </div>
-              <section className="buttons">
-                <button className="secondary" onClick={()=>handleBuy()}><MdAddShoppingCart/>Adicionar ao carrinho</button>
-                <button className="primary" onClick={()=>handleBuy(true)}>Comprar agora</button>
-              </section>
-            </aside>
-          </div>
-          <div className="moreComics">
-            <h3>Mais Obras</h3>
-            <div className="row">
-              {comics?.map((c) => {
-                return (
-                  <figure className="thumbnail" key={c.id}>
-                    <img
-                      src={
-                        c.thumbnail.path +
-                        "/portrait_medium" +
-                        "." +
-                        c.thumbnail.extension
-                      }
-                      alt={c.title}
-                    />
-                    <b>{c.title}</b>
-                  </figure>
-                );
-              })}
+  if (!loading) {
+    const comicCover = details.comic?.images.length ? `${details.comic.images[0].path}.${details.comic.images[0].extension}`:`${details.comic?.thumbnail.path}/portrait_uncanny.${details.comic?.thumbnail.extension}`
+    const characterCover = `${details.character?.thumbnail.path}.${details.character?.thumbnail.extension}`
+
+    return (
+      <main className="mainDetails">
+        {details.comic || details.character ? (
+          <div className="container">
+            <nav onClick={handleNavigate}>
+              <RxChevronLeft color="#FF8100" />
+              Voltar
+            </nav>
+            <div className="comic">
+              <img
+                className="cover"
+                src={details.comic?comicCover:characterCover}
+                alt="comic cover"
+              />
+              <aside>
+                <section className="title">
+                  <h2>{details.comic?details.comic.title:details.character?.name}</h2>
+                  {details.comic?<p>{"$ "+details.comic.prices[0].price}</p>:<></>}
+                </section>
+                <div className="details">
+                  {details.comic?<ComicRows comic={details.comic}/>:<></>}
+                  {details.character?<CharacterRows character={details.character}/>:<></>}
+                  <article className="thumbnails">
+                  {details.comic?<>
+                      <h3>Personagens da obra</h3>
+                      <div className="row">
+                        {details.characters?details.characters.map((c) => {
+                          return <Thumbnail key={c.id} name={c.name} img={c.thumbnail} format="standard_medium" round/>;
+                        }):<p>Nenhum Personagem</p>}
+                      </div>
+                    </>:
+                    <>
+                      <h3>Histórias</h3>
+                      <div className="row">
+                        {details.characterComics.map((c)=>{
+                          return <Thumbnail key={c.id} name={c.title} img={c.thumbnail} format="portrait_medium" round={false}/>;
+                        })}
+                      </div>
+                    </>
+                  }
+                  </article>
+                </div>
+                {details.comic?
+                  <section className="buttons">
+                    <button className="secondary" onClick={() => handleBuy()}>
+                      <MdAddShoppingCart />
+                      Adicionar ao carrinho
+                    </button>
+                    <button className="primary" onClick={() => handleBuy(true)}>
+                      Comprar agora
+                    </button>
+                  </section>
+                :<></>}
+              </aside>
             </div>
-            <footer>Todos os direitos reservados a UOL Comics 2024</footer>
+            <div className="moreComics">
+              <h3>Mais Obras</h3>
+              <div className="row">
+                {details.comics?.map((c) => {
+                  return (
+                    <figure className="thumbnail" key={c.id}>
+                      <img
+                        src={
+                          c.thumbnail.path +
+                          "/portrait_medium" +
+                          "." +
+                          c.thumbnail.extension
+                        }
+                        alt={c.title}
+                      />
+                      <b>{c.title}</b>
+                    </figure>
+                  );
+                })}
+              </div>
+              <footer>Todos os direitos reservados a UOL Comics 2024</footer>
+            </div>
           </div>
-        </div>
-      ) : (
+        ) : (
+          <div className="loader">
+            <h4>Information not found</h4>
+          </div>
+        )}
+      </main>
+    );
+  } else {
+    return (
+      <main className="mainDetails">
         <div className="loader">
-          {noComic ? <h4>Comic not found</h4>:<img src={loading} alt="loading" />}
+          <img src={loader} alt="loading" />
         </div>
-      )}
-    </main>
+      </main>
+    );
+  }
+}
+
+function Thumbnail({ name,img,format,round }: Readonly<{ name: string,img:{path:string, extension:string},format:string, round:boolean }>) {
+  return (
+    <figure className="character" key={name}>
+      <img src={img.path +`/${format}.`+img.extension} alt="thumbnail" className={round?'round':''}/>
+      <b>{name}</b>
+    </figure>
+  )
+}
+
+function ComicRows({ comic }: Readonly<{ comic: Comic }>) {
+  return (
+    <>
+      <article>
+        <h3>Publicado em</h3>
+        <p>{comic.dates[0].date.slice(0, 4)}</p>
+      </article>
+      <article>
+        <h3>Núm. de Páginas</h3>
+        <p>{comic.pageCount}</p>
+      </article>
+      <article>
+        <h3>Autor</h3>
+        <p>{comic.creators.items[0]?.name}</p>
+      </article>
+      <article >
+        <h3>Série</h3>
+        <p>{comic.series.name}</p>
+      </article>
+    </>
+  );
+}
+
+function CharacterRows({ character }: Readonly<{ character: Character }>) {
+  return (
+    <>
+      <div className="mergedRow">
+        <article>
+          <h3>Criado em</h3>
+          <p>{new Date(character.modified).getFullYear()}</p>
+        </article>
+        <article>
+          <h3>Histórias</h3>
+          <p>{character.stories.items.length}</p>
+        </article>
+        <article>
+          <h3>núm. de séries</h3>
+          <p>{character.series.items.length}</p>
+        </article>
+      </div>
+      <article className="extendRow">
+        <h3>Descrição</h3>
+        <p>{character.description || "No description available."}</p>
+      </article>
+    </>
   );
 }
