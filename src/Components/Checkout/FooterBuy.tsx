1df2fb2e0fa8402849787dccface5149212fe863
biancassc <biancassc26@gmail.com>
2024-10-05 22:31:33 -0300

    feat: add animating com setTimeOut 300ms

--- src/Components/Checkout/FooterBuy.tsx
+++ src/Components/Checkout/FooterBuy.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { useCar } from "../Checkout/CheckoutLogic";
 import { useNavigate } from "react-router-dom";
 import "./StylesCar/FooterBuy.css";
 
+
 const FooterBuy: React.FC = ()=>{
     const {carItems} = useCar();
     const navigate = useNavigate();
-     
+    const [isAnimating, setIsAnimating] = useState(false);
     const handleBuy = ()=>{
         if (carItems.length > 0) {
+            setIsAnimating(true);
+            setTimeout(() =>{  
             navigate('/buyInfo');
+            },300);
         }
         else {
             navigate('/EmptyCar');
@@ -17,7 +21,7 @@ const FooterBuy: React.FC = ()=>{
     };
     return (
         <footer className="footer">
-            <button onClick ={handleBuy} className="buy">Comprar</button>
+            <button onClick ={handleBuy} className={`buy ${isAnimating ? "dissolve" : ""}`}>Comprar</button>
         </footer>
     )
 };
